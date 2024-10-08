dbce72a7e5f3aee28f01033a86dfb6649b7fd995
BoatGT-9 <twz192545@gmail.com>
2024-10-08 17:29:29 +0700

    update Accuont file success fully

--- cypress/e2e/Account/EditAccount.cy.js
+++ cypress/e2e/Account/EditAccount.cy.js
@@ -1,80 +1,126 @@
-const BASE_URL = "http://warering.online/";
-describe("Edit account", () => {
+import { login } from "./spec.cy.js";
+describe("Profile", () => {
+  it("TC7001 view account ", () => {
+    login("boat3", "19092545Boat");
+    // view account
+    cy.get("#user-profile-image").click();
+    cy.get("#account-user-drawer-nav-link").click();
+    cy.wait(2000);
+    cy.screenshot();
+  });
+
   // เปลี่ยนชื่อ สำเร็จ
-  it("edit account firstName and lastName", () => {
-    cy.visit(BASE_URL);
-    // login account
-    cy.contains("Sign In").click();
-    cy.get("div:nth-child(1) > .relative > #username").type("boat3");
-    cy.get("div:nth-child(1) > .relative > #password").type("19092545Boat");
-    cy.get("#setup-user-drawer-submit").click();
+  it("TC7002 edit account firstName and lastName", () => {
+    login("boat3", "19092545Boat");
     // edit Name
     cy.get("#user-profile-image").click();
     cy.get("#edit-profile-drawer-nav-link").click();
     cy.get("#firstName").click();
     cy.get("#firstName").clear();
-    cy.get("#firstName").type(`User_firstName`);
+    cy.get("#firstName").type(`Surapong`);
     cy.get("#lastName").click();
     cy.get("#lastName").clear();
-    cy.get("#lastName").type("User_lastName");
-    // รอใส่ตัวสมบูรณ์
+    cy.get("#lastName").type("Keaynin");
     cy.get("#save-update-new-user-info-btn").click();
+    cy.contains("Updated your information").should('exist')
     cy.screenshot();
   });
 
-  // // ชื่อไม่แมท
-  it("Edit firstName not match pattern", () => {
-    cy.visit(BASE_URL);
-    // login account
-    cy.contains("Sign In").click();
-    cy.get("div:nth-child(1) > .relative > #username").type("boat3");
-    cy.get("div:nth-child(1) > .relative > #password").type("19092545Boat");
-    cy.get("#setup-user-drawer-submit").click();
-    // edit Name
+  // ชื่อไม่แมท
+  it("TC7003 Edit firstName not match pattern", () => {
+    login("boat3", "19092545Boat");
     cy.get("#user-profile-image").click();
     cy.get("#edit-profile-drawer-nav-link").click();
     cy.get("#firstName").click();
     cy.get("#firstName").clear();
-    cy.get("#firstName").type(`User_firstName${new Date().getSeconds()}`);
+    cy.get("#firstName").type(`Boat_r`);
     cy.get("#lastName").click();
     cy.get("#lastName").clear();
-    cy.get("#lastName").type("User_lastName");
-    // รอใส่ตัวสมบูรณ์
+    cy.get("#lastName").type("Keaynin");
     cy.get("#save-update-new-user-info-btn").click();
+    cy.contains("firstName must match ").should('exist')
     cy.screenshot();
   });
 
-  //   if (
-  //     ("file Upload",
-  //     function () {
-  //       it("File upload", function () {
-  //         cy.visit("https://tinypng.com/");
-  //       });
-  //     })
-  //   )
+  // นามสกุลไม่แมท
+  it("TC7004 Edit LastName not match pattern", () => {
+    login("boat3", "19092545Boat");
+    cy.get("#user-profile-image").click();
+    cy.get("#edit-profile-drawer-nav-link").click();
+    cy.get("#firstName").click();
+    cy.get("#firstName").clear();
+    cy.get("#firstName").type("boat");
+    cy.get("#lastName").click();
+    cy.get("#lastName").clear();
+    cy.get("#lastName").type("Keaynin_d");
+    cy.get("#save-update-new-user-info-btn").click();
+    cy.contains("lastName must match ").should('exist')
+    cy.screenshot();
+  });
 
+  it("TC7005 User can change password ",()=>{
+    login("kran","Kumpa12345");
+    cy.get("#user-profile-image").click();
+    cy.get("#edit-profile-drawer-nav-link").click();
+    cy.get("#change-password-btn").click();
+    cy.get("#current_password").type("Kumpa12345");
+    cy.get("#new_password").type("Kran12345");
+    cy.get("#confirm_new_password").type("Kran12345");
+    cy.contains("Updated Password").click();
+    cy.contains("Updated your password").should('exist');
+    cy.screenshot();
+  })
+
+  it("TC7006 User not can change password  ",()=>{
+    login("kran","Kran12345");
+    cy.get("#user-profile-image").click();
+    cy.get("#edit-profile-drawer-nav-link").click();
+    cy.get("#change-password-btn").click();
+    // cy.get("#current_password").type("Kumpa12345");
+    cy.get("#new_password").type("Kran12345");
+    cy.get("#confirm_new_password").type("Kran12345");
+    cy.contains("Updated Password").click();
+    cy.contains("current password is required").should('exist');
+    cy.screenshot();
+  })
+
+  it("TC7007 User not can change password ",()=>{
+    login("kran","Kran12345");
+    cy.get("#user-profile-image").click();
+    cy.get("#edit-profile-drawer-nav-link").click();
+    cy.get("#change-password-btn").click();
+    cy.get("#current_password").type("Kumpa12345");
+    // cy.get("#new_password").type("Kran12345");
+    cy.get("#confirm_new_password").type("Kran12345");
+    cy.contains("Updated Password").click();
+    cy.contains("new password is required").should('exist');
+    cy.screenshot();
+  })
+
+  it("TC7008 User not can change password ",()=>{
+    login("kran","Kran12345");
+    cy.get("#user-profile-image").click();
+    cy.get("#edit-profile-drawer-nav-link").click();
+    cy.get("#change-password-btn").click();
+    cy.get("#current_password").type("Kumpa12345");
+    cy.get("#new_password").type("Kran12345");
+    // cy.get("#confirm_new_password").type("Kran12345");
+    cy.contains("Updated Password").click();
+    cy.contains("confirm new password is required").should('exist');
+    cy.screenshot();
+  })
   
-  //   เปลี่ยนรูปภาพโปรไฟล์
-  it("Chang Photo Profile", () => {
-    cy.visit(BASE_URL);
-    // login
-    cy.contains("Sign In").click();
-    cy.get("div:nth-child(1) > .relative > #username").type("boat3");
-    cy.get("div:nth-child(1) > .relative > #password").type("19092545Boat");
-    cy.wait(5000)
-    cy.get("#setup-user-drawer-submit").click();
-    // กดเข้าหน้า account
+  it("TC7009 PasswordNew  to weak ",()=>{
+    login("kran","Kran12345");
     cy.get("#user-profile-image").click();
     cy.get("#edit-profile-drawer-nav-link").click();
-
-    cy.get("#update-user-profile-btn").click();
-    cy.wait(2000)
-    cy.get("#file-drop-zone").click();
-    cy.wait(5000)
-  });
-  // //   ลบรูปภาพโปรไฟล์
-  // it("Delete Photo Profile", () => {
-  //   cy.get("#remove-user-profile-btn").click();
-  // });
+    cy.get("#change-password-btn").click();
+    cy.get("#current_password").type("Kumpa12345");
+    cy.get("#new_password").type("12345");
+    cy.get("#confirm_new_password").type("12345");
+    cy.contains("Updated Password").click();
+    cy.contains("PasswordNew too weak").should('exist');
+    cy.screenshot();
+  })
 
 });
