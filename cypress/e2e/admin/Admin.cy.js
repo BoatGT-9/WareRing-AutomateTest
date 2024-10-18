394c67607e0338642eccece67d9392e1069c35e6
BoatGT-9 <twz192545@gmail.com>
2024-10-18 10:26:09 +0700

    Admin success fully

--- cypress/e2e/admin/Admin.cy.js
+++ cypress/e2e/admin/Admin.cy.js
@@ -1,32 +1,71 @@
 const BASE_URL = "http://localhost:5173";
 describe("Admin test", () => {
   // แอ็ดมิน ล็อคอินสำเร็จ
-  it("Admin login Success Fully ", () => {
+  it("TC9001 Admin login Success Fully ", () => {
     cy.visit(BASE_URL);
-    cy.get("#open-big-login-drawer").click();
-    cy.get("div:nth-child(1) > .relative > #username").type("");
-    cy.get("div:nth-child(1) > .relative > #password").type("");
-    cy.get("div:nth-child(1) > #setup-user-submit").click();
+    cy.get('#toggle-big-login-landing-drawer-btn').click();
+    cy.get('#username').type("AdminWareringCaxknsa");
+    cy.get('#password').type("kmsad9ASdjas0LSJWd9iaa");
+    cy.get('#setup-user-drawer-submit').click();
+    cy.wait(1000);
+    cy.contains("User List").should('exist');
     cy.screenshot();
   });
-  //   แอ็ดมินใส่ชื่อไม่ถูกต้อง
-  it("Login to fail User Not math", () => {
+  //   ไม่มีบัญชีใน
+  it("TC9002 Login to fail User Not found", () => {
     cy.visit(BASE_URL);
-    cy.contains("Sign In").click();
-    cy.get("div:nth-child(1) > .relative > #username").type();
-    cy.get("div:nth-child(1) > .relative > #password").type();
-    cy.get("div:nth-child(1) > #setup-user-submit").click();
-    cy.contains("username must match").should("exist");
+      cy.get('#toggle-big-login-landing-drawer-btn').click();
+      cy.get('#username').type("AdminWareringbrabra");
+      cy.get('#password').type("kmsad9ASdjas0LSJWd9iaa");
+      cy.get('#setup-user-drawer-submit').click();
+    cy.contains("not found user").should("exist");
     cy.screenshot();
   });
+
   //   แอ็ดมิน ใส่รหัสผ่านไม่ถูกต้อง
-  it("Login to fail Password Not Math", () => {
+  it("TC9003 Login to fail Password Not Math", () => {
     cy.visit(BASE_URL);
-    cy.contains("Sign In").click();
-    cy.get("div:nth-child(1) > .relative > #username").type();
-    cy.get("div:nth-child(1) > .relative > #password").type();
-    cy.get("div:nth-child(1) > #setup-user-submit").click();
-    cy.contains("password is incorrect").should("exist");
-    cy.screenshot
+    cy.get("#toggle-big-login-landing-drawer-btn").click();
+    cy.get("#username").type("AdminWareringCaxknsa");
+    cy.get("#password").type("kmsad9ASdjas0LSJWd9iaasa");
+    cy.get("#setup-user-drawer-submit").click();
+    cy.contains("Password is incorrect").should("exist");
+    cy.wait(1000);
+    cy.screenshot();
   });
+
+  // ล็อคอินไม่สำเร็จ ไม่มีชื่อผู้ใช้
+  it("TC9004 Login to fail Not have Username", () => {
+    cy.visit(BASE_URL);
+    cy.get('#toggle-big-login-landing-drawer-btn').click();
+    cy.get('#password').type("kmsad9ASdjas0LSJWd9iaa");
+    cy.get('#setup-user-drawer-submit').click();
+    cy.wait(1000);
+    cy.contains("Username is required").should('exist');
+    cy.screenshot();
+  });
+
+  // ล็อคอินไม่สำเร็จ ไม่มีรหัสผ่าน
+  it("TC9005 Login to fail Not have Password  ", () => {
+    cy.visit(BASE_URL);
+    cy.get('#toggle-big-login-landing-drawer-btn').click();
+    cy.get('#username').type("AdminWareringCaxknsa");
+    cy.get('#setup-user-drawer-submit').click();
+    cy.wait(1000);
+    cy.contains("Password is required").should('exist');
+    cy.screenshot();
+  });
+
+  it("TC9006 Login to fail Password has less than 8 characters ", () => {
+    cy.visit(BASE_URL);
+    cy.get('#toggle-big-login-landing-drawer-btn').click();
+    cy.get('#username').type("AdminWareringCaxknsa");
+    cy.get('#password').type("kmsad9");
+    cy.get('#setup-user-drawer-submit').click();
+    cy.wait(1000);
+    cy.contains("password must be longer than or equal to 8 characters").should('exist');
+    cy.screenshot();
+  });
+
+
 });
--- cypress/e2e/admin/AdminIsActive.cy.js
+++ cypress/e2e/admin/AdminIsActive.cy.js
@@ -1,14 +1,27 @@
-describe("Admin is Active and in Active", () => {
-  it("Admin is Active", () => {
-    cy.visit("/Admin");
+// const BASE_URL = "http://localhost:5173";
+import { login } from "./Space.cy";
+describe("Admin is Active User ", () => {
+  it("TC9007 Admin is Banned User", () => {
+    login("AdminWareringCaxknsa", "kmsad9ASdjas0LSJWd9iaa");
     cy.contains("User List").should("exist");
-    cy.get("#.sm\\3A flex:nth-child(1) .MuiButtonBase-root").click();
-    cy.contains("IN ACTIVE").should("exist");
+    cy.contains("Active").click();
+    cy.contains("Banned").should("exist");
+    cy.screenshot();
   });
-  it("Admin in Active", () => {
-    cy.visit("/Admin");
+
+  it("TC9008 Admin is Active User ", () => {
+    login("AdminWareringCaxknsa", "kmsad9ASdjas0LSJWd9iaa");
     cy.contains("User List").should("exist");
-    cy.get("#.sm\\3A flex:nth-child(1) .MuiButtonBase-root").click();
-    cy.contains("IS ACTIVE").should("exist");
+    cy.contains("Banned").click();
+    cy.contains("Active").should("exist");
+    cy.screenshot();
+  });
+
+  it("TC9009 Admin can search user", () => {
+    login("AdminWareringCaxknsa", "kmsad9ASdjas0LSJWd9iaa");
+    cy.contains("User List").should("exist");
+    cy.get("#search_user").type("boat");
+    cy.contains("boat").should("exist");
+    cy.screenshot();
   });
 });
