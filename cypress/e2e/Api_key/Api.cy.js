ecb6b50097f2012469980dc0832ee0845e10870f
BoatGT-9 <twz192545@gmail.com>
2024-10-25 14:38:49 +0700

    success

--- cypress/e2e/Api_key/Api.cy.js
+++ cypress/e2e/Api_key/Api.cy.js
@@ -7,36 +7,24 @@ describe("Api-key", () => {
     cy.get("#create-api-key-btn").click();
     cy.get("#name").type("บ้านไฮเทค");
     cy.get("#description").type("รวมอุปกรณ์ไฮเทค");
-    cy.get("#expireIn").type("2024-10-18");
+    cy.get("#expireIn").type("2024-10-25");
     cy.get("#submit-create-api-key-btn").click();
     cy.contains("Your API key was created").should("exist");
     cy.screenshot();
   });
 
-  it("TC10.002 Admin can export JSON file", () => {
-    login("AdminWareringCaxknsa", "kmsad9ASdjas0LSJWd9iaa");
-    cy.get("#toggle-nav-links-dialog-btn").click();
-    cy.contains("API Keys").click();
-    cy.get("#create-api-key-btn").click();
-    cy.get("#name").type("บ้านไฮเทค");
-    cy.get("#description").type("รวมอุปกรณ์ไฮเทค");
-    cy.get("#expireIn").type("2024-10-18");
-    cy.get("#submit-create-api-key-btn").click();
-    cy.get("#download-api-key-json-btn").click();
-    cy.screenshot();
-  });
-
-  it("TC10.003 Admin can Delete key", () => {
+  
+  it("TC10.002 Admin can Delete key", () => {
     login("AdminWareringCaxknsa", "kmsad9ASdjas0LSJWd9iaa");
     cy.get("#toggle-nav-links-dialog-btn").click();
     cy.contains("API Keys").click();
     cy.contains("Delete").click();
-    cy.get("#confirm-delete-api-key").click("");
+    cy.get("#confirm-delete-api-key").click();
     cy.contains("Deleted your API key").should("exist");
     cy.screenshot();
   });
 
-  it("TC10.004 Admin can't Create API KEY no have name  ",()=>{
+  it("TC10.003 Admin can't Create API KEY no have name  ",()=>{
     login("AdminWareringCaxknsa", "kmsad9ASdjas0LSJWd9iaa");
     cy.get("#toggle-nav-links-dialog-btn").click();
     cy.contains("API Keys").click();
@@ -48,7 +36,7 @@ describe("Api-key", () => {
     cy.screenshot();
   });
 
-  it("TC10.005 Admin can't Create API KEY no have description ",()=>{
+  it("TC10.004 Admin can't Create API KEY no have description ",()=>{
     login("AdminWareringCaxknsa", "kmsad9ASdjas0LSJWd9iaa");
     cy.get("#toggle-nav-links-dialog-btn").click();
     cy.contains("API Keys").click();
@@ -60,7 +48,7 @@ describe("Api-key", () => {
     cy.screenshot();
   })
 
-  it("TC10.006 Admin can search Api key",()=>{
+  it("TC10.005 Admin can search Api key",()=>{
     login("AdminWareringCaxknsa", "kmsad9ASdjas0LSJWd9iaa");
     cy.get("#toggle-nav-links-dialog-btn").click();
     cy.contains("API Keys").click();
@@ -68,7 +56,8 @@ describe("Api-key", () => {
     cy.contains("จุมเม้").should("exist");
     cy.screenshot();
   })
-  it("TC10.007 Admin can filter Sort by Date Oldest",()=>{
+
+  it("TC10.006 Admin can filter Sort by Date Oldest",()=>{
     login("AdminWareringCaxknsa", "kmsad9ASdjas0LSJWd9iaa");
     cy.get("#toggle-nav-links-dialog-btn").click();
     cy.contains("API Keys").click();
@@ -77,14 +66,25 @@ describe("Api-key", () => {
     cy.screenshot();
   });
 
-  it("TC10.008 Admin can filter Sort by Date latest",()=>{
+  it("TC10.007 Admin can filter Sort by Date latest",()=>{
     login("AdminWareringCaxknsa", "kmsad9ASdjas0LSJWd9iaa");
     cy.get("#toggle-nav-links-dialog-btn").click();
     cy.contains("API Keys").click();
-    cy.get("#sort-by-date-select").select("latest");
-    cy.contains("18/10/2567").should("exist");
+    cy.get("#sort-by-date-select").select("Latest");
+    cy.contains("25/10/2567").should("exist");
     cy.screenshot();
   });
 
-
+  it("TC10.008 Admin can export JSON file", () => {
+    login("AdminWareringCaxknsa", "kmsad9ASdjas0LSJWd9iaa");
+    cy.get("#toggle-nav-links-dialog-btn").click();
+    cy.contains("API Keys").click();
+    cy.get("#create-api-key-btn").click();
+    cy.get("#name").type("บ้านไฮเทค");
+    cy.get("#description").type("รวมอุปกรณ์ไฮเทค");
+    cy.get("#expireIn").type("2024-10-18");
+    cy.get("#submit-create-api-key-btn").click();
+    cy.get("#download-api-key-json-btn").click();
+    cy.screenshot();
+  });
 });
